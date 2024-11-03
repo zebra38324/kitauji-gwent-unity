d4194713edd4e3d563b0515829fecb31c53a144d
zebra38324 <okiya_subaru@126.com>
2024-11-03 10:06:22 +0800

    add CardCollectionModelTest

---
+++ Assets/Editor/Test/Card/CardCollectionModelTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+public class CardCollectionModelTest
+{
+    [Test]
+    public void Demo()
+    {
+        CardCollectionModel cardCollectionModel = new CardCollectionModel();
+        for (int i = 0; i < 10; i++) {
+            CardInfo cardInfo = new CardInfo();
+            cardInfo.id = i;
+            cardCollectionModel.AddCard(cardInfo);
+        }
+        cardCollectionModel.RemoveCard(3);
+        cardCollectionModel.RemoveCard(10); // out of range
+    }
+}
