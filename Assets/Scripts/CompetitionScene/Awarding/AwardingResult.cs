ca3ef1dd2612057b1a9c3f08a92f68a789a448fb
zebra38324 <1037280663@qq.com>
2025-07-21 21:07:36 +0800

    调整颁奖仪式展示

---
+++ Assets/Scripts/CompetitionScene/Awarding/AwardingResult.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AwardingResult : MonoBehaviour
+{
+    public TextMeshProUGUI title;
+
+    public GameObject table;
+
+    public GameObject rowPrefab;
+
+    private CompetitionContextModel context;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Show(CompetitionContextModel context_)
+    {
+        context = context_;
+        title.text = $"【{CompetitionBase.LEVEL_TEXT[(int)context.currnetLevel]}】竞赛结果";
+
+        var firstRow = Instantiate(rowPrefab, table.gameObject.transform);
+        if (context.currnetLevel == CompetitionBase.Level.National) {
+            firstRow.GetComponent<AwardingResultTableRow>().HidePromote();
+        }
+        firstRow.GetComponent<AwardingResultTableRow>().UpdateText("序号",
+            "学校",
+            "奖项",
+            "代表");
+
+        int index = 0;
+        foreach (var team in context.teamDict.Values) {
+            string prefix = "";
+            string suffix = "";
+            if (team.name == context.playerName) {
+                prefix = "<color=green>";
+                suffix = "</color>";
+            }
+            var row = Instantiate(rowPrefab, table.gameObject.transform);
+            row.GetComponent<AwardingResultTableRow>().UpdateText($"{prefix}{index + 1}{suffix}",
+                $"{prefix}{team.name}{suffix}",
+                $"{prefix}{CompetitionBase.PRIZE_TEXT[(int)team.prize].Substring(0, 1)}{suffix}",
+                team.prize == CompetitionBase.Prize.GoldPromote ? "〇" : "");
+            index += 1;
+        }
+    }
+}
---
+++ Assets/Scripts/CompetitionScene/Awarding/AwardingResultTableRow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AwardingResultTableRow : MonoBehaviour
+{
+    public TextMeshProUGUI num;
+
+    public TextMeshProUGUI schoolName;
+
+    public TextMeshProUGUI prize;
+
+    public TextMeshProUGUI promote;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateText(string num_, string schoolName_, string prize_, string promote_)
+    {
+        num.text = num_;
+        schoolName.text = schoolName_;
+        prize.text = prize_;
+        promote.text = promote_;
+    }
+
+    public void HidePromote()
+    {
+        promote.gameObject.SetActive(false);
+    }
+}
