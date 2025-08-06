4e6283ab2880d6b9952ede9bbf87ad5eeaa4d5c1
zebra38324 <1037280663@qq.com>
2025-08-06 22:29:02 +0800

    fix: 竞赛模式进度读取异常

--- Assets/Scripts/CompetitionScene/Model/CompetitionGameInfoModel.cs
+++ Assets/Scripts/CompetitionScene/Model/CompetitionGameInfoModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,17 +7,26 @@ using System.Collections.Generic;
 [Serializable]
 public class CompetitionGameInfoModel
 {
+    [JsonProperty]
     public string selfName { get; private set; } // 本方名称
 
+    [JsonProperty]
     public string enemyName { get; private set; } // 对手名称
 
+    [JsonProperty]
     public int selfScore { get; private set; } // 本方得分
 
+    [JsonProperty]
     public int enemyScore { get; private set; } // 对手得分
 
+    [JsonProperty]
     public bool selfWin { get; private set; } // 本方是否获胜
 
+    [JsonProperty]
     public bool hasFinished { get; private set; } = false; // 比赛是否结束
+
+    // 添加无参构造函数（反序列化必需）
+    public CompetitionGameInfoModel() { }
 
     public CompetitionGameInfoModel(string selfName, string enemyName)
     {
